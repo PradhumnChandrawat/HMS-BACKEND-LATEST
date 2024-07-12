eabd735aa3f2a233ed30471158be31d049f56bcf
Pradhumn Chandrawat <chandrawatpradhumn@gmail.com>
2024-07-12 23:35:13 +0530

    after some changes

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-import app from "./express.js";
-import { v2 as cloudinary } from "cloudinary";
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { dbConnection } from "./database/dbConnection.js";
+import messageRouter from "./routers/messageRouter.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./routers/userRouter.js";
+import appointmentRouter from "./routers/appointmentRouter.js";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+const app = express();
+config({ path: "./config.env" });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
-});
+// middleware for connecting frontend to the backend
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// middleware for reading cookie values that are sent from the client's browser(in request header) to the serve(access these cookies as an Object)
+app.use(cookieParser());
+
+// middleware for parsing data to JSON
+app.use(express.json());
+
+// This middleware allows the server to read and parse URL-encoded data sent through HTML form submissions, making this data available in req.body.
+app.use(express.urlencoded({ extended: true }));
+
+// middleware to handle file uploads
+app.use(
+  fileUpload({
+    // This tells the middleware to save the uploaded files as temporary files on the server instead of storing them in memory
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
